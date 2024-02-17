8d6d8a7562d21612aa1fb386daaca69d3c8e0049
Jose Sorli <jose.sorli.esteve@hotmail.com>
2024-02-17 12:25:28 +0100

    V4. Grafo funcionando con 3 fragments y argumentos. Todo comentado 2

--- app/src/main/java/com/example/ejemplofragment/TercerFragment.kt
+++ app/src/main/java/com/example/ejemplofragment/TercerFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 
 class TercerFragment : Fragment() {
@@ -18,12 +20,30 @@ class TercerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         //Creamos esta variable para acceder fácilmente a las vistas del fragment
+        //Se está creando una vista desde el archivo xml indicado.
         val root = inflater.inflate(R.layout.fragment_tercer, container, false)
 
+        val btnNav = root.findViewById<Button>(R.id.btnFragmentClick3)
+        val btnNav2 = root.findViewById<Button>(R.id.btnFragmentClick4)
         //Si es la primera vez que se crea la vista, metemos en LogCat el valor del argumento
         if(savedInstanceState == null){
             Log.i("LOGFRAGMENT :",args.provincia.toString())
         }
+        btnNav.setOnClickListener {
+            //llamamos a la acción , no al fragment
+            //En este caso, al pulsar el botón, iremos al primer fragment
+            val accion = TercerFragmentDirections.actionTercerFragmentToPrimerFragmento()
+
+            findNavController().navigate(accion)
+        }
+        btnNav2.setOnClickListener {
+            //llamamos a la acción , no al fragment
+            //En este caso, al pulsar el botón, iremos al SEGUNDO fragment
+            val accion = TercerFragmentDirections.actionTercerFragmentToSegundoFragment()
+
+            findNavController().navigate(accion)
+        }
+
         return root
     }
 }
